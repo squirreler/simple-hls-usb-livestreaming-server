2b800761e5fa549a25e707216d650c3df2efe257
Daniel B <dbbemailme@gmail.com>
2024-07-23 22:12:06 -0700

    finished routing, moved files around, got streaming to work

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask import render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
-
+# app.config["USE_X_SENDFILE"] = True
 @app.route("/")
 def render_video_page():
-    return render_template(index.html);
+    return render_template("index.html")
+
+@app.route("/video-files/playlist.m3u8")
+def send_manifest(): 
+    return send_from_directory("./video-files/","playlist.m3u8")
+#converts to string by default
+@app.route("/video-files/<tsfilename>")
+def send_video(tsfilename): 
+    print(tsfilename)
+    return send_from_directory("./video-files/", tsfilename)
